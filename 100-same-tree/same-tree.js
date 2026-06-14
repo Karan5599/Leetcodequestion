b7fa645de8b88aad7d5211afec0f2babba8a4fb3
Karan Kumar <karanguptta5858@gmail.com>
2026-06-14 17:54:24 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (40.21%) - LeetSync

--- 100-same-tree/same-tree.js
+++ 100-same-tree/same-tree.js
@@ -11,22 +11,30 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
+// var isSameTree = function (p, q) {
+//     let curr = [p, q];
+//     while (curr.length) {
+//         let p1 = curr.shift();
+//         let p2 = curr.shift();
+//         if (p1 === null && p2 === null) {
+//             continue;
+//         }
+//         if (p1 === null || p2 === null) {
+//             return false;
+//         }
+//         if (p1.val !== p2.val) {
+//             return false;
+//         }
+//         curr.push(p1.left, p2.left)
+//         curr.push(p1.right, p2.right)
+//     }
+//     return true;
+// };
+
 var isSameTree = function (p, q) {
-    let curr = [p, q];
-    while (curr.length) {
-        let p1 = curr.shift();
-        let p2 = curr.shift();
-        if (p1 === null && p2 === null) {
-            continue;
-        }
-        if (p1 === null || p2 === null) {
-            return false;
-        }
-        if (p1.val !== p2.val) {
-            return false;
-        }
-        curr.push(p1.left, p2.left)
-        curr.push(p1.right, p2.right)
-    }
-    return true;
-};
+   if(!p && !q) return true;
+   if(!p || !q) return false;
+    return(p.val == q.val) &&
+    isSameTree(p.left, q.left) &&
+    isSameTree(p.right, q.right)
+}
